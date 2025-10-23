adef622dd6f8d69fea56c88f937e3390798155fd
Arian <arian@web-essentials.co>
2025-10-23 16:29:36 +0700

    [TASK] Upload somethings
    refs ARIAN-01

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Item
 
-# Create your views here.
+def home(request):
+    items = Item.objects.all()
+    return render(request, 'myapp/home.html', {'items': items})
+
+# def home(request):
+#     items = Item.objects.all()
+#     items_list = ", ".join([item.name for item in items])
+#     return HttpResponse(f"Items: {items_list}")
